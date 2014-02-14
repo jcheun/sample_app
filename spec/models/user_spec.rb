6ec911a3141a8962c53dc6a873df63781b933555
Jesse Cheun <jcheun@ucsc.edu>
2014-02-14 00:32:05 -0800

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe User do
-before do
+  before do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
@@ -63,18 +63,19 @@ before do
     it { should_not be_valid }
    end
 
-   describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
                      password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
   end
-  it { should_not be_valid }
-end
 
-describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+  
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
